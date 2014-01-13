8e723796d8c3175b540dcf87620a3098df0af853
Andrea Cantafio <kant93@live.it>
2014-01-13 14:00:59 +0100

    Fixed bootstrap url and port

--- template/bootstrap.dart
+++ template/bootstrap.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 main() {
-  HttpServer.bind('127.0.0.1', 8080).then((server) {
+  final env = Platform.environment;
+  HttpServer.bind(env['OPENSHIFT_DAPP_IP'], env['OPENSHIFT_DAPP_PORT']).then((server) {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world');
       request.response.close();
