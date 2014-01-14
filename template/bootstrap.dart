f25db72c536ad5bc36ea0c0fa7c24833ad85258a
Andrea Cantafio <kant93@live.it>
2014-01-14 09:07:25 +0100

    port conversion to int added

--- template/bootstrap.dart
+++ template/bootstrap.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 main() {
   final env = Platform.environment;
-  HttpServer.bind(env['OPENSHIFT_DAPP_IP'], env['OPENSHIFT_DAPP_PORT']).then((server) {
+  HttpServer.bind(env['OPENSHIFT_DART_IP'], int.parse(env['OPENSHIFT_DART_PORT'], onError: (_) => 8080)).then((server) {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world');
       request.response.close();
